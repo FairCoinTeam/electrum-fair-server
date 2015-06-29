3905b424acd75c9a9dcdb3bec8b16c2604ab9283
thokon00 <tom@fair-coin.org>
2015-06-29 22:37:52 +0200

    port to FairCoin

--- src/networks.py
+++ src/networks.py
@@ -2,13 +2,13 @@
 
 params = {
     'bitcoin_main': {
-        'pubkey_address': 0,
-        'script_address': 5,
-        'genesis_hash': '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+        'pubkey_address': 95,
+        'script_address': 36,
+        'genesis_hash': 'f1ae188b0c08e296e45980f9913f6ad2304ff02d5293538275bacdbcb05ef275'
     },
     'bitcoin_test': {
         'pubkey_address': 111,
         'script_address': 196,
-        'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
+        'genesis_hash': '5df7281a262d9a6251e65d12bdb78c6a69041c7d28e209df2a7719059de3088c'
     }
 }
